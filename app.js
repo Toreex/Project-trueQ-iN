6d7f9eaef1263d33039d2bba0bd5e3571013d42d
Didac Tey <t_didac18@hotmail.com>
2017-08-28 16:52:18 +0200

    add files .pug

--- app.js
+++ app.js
@@ -4,11 +4,41 @@ const path = require('path')
 const PORT = process.env.PORT || 3000
 
 const app = express()
+
 app.set('view engine', 'pug')
+
 app.use(express.static(path.join(__dirname, 'public')))
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+app.get('/resultados', (req, res) => {
+  res.render('resultados')
+})
+
+app.get('/entrar', (req, res) => {
+  res.render('entrar')
+})
+
+app.get('/registrar', (req, res) => {
+  res.render('registrar')
+})
+
+app.get('/perfilUser', (req, res) => {
+  res.render('perfilUser')
+})
+
+app.get('/userProduct', (req, res) => {
+  res.render('userProduct')
+})
+
+app.get('/userIn', (req, res) => {
+  res.render('userIn')
+})
+
+app.get('/productInfo', (req, res) => {
+  res.render('productInfo')
 })
 
 app.listen(PORT)
